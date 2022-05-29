3b32bf01e78b239b8c9e27533da8ed974ee68736
Natan Barboza <natanbarboza084@gmail.com>
2022-05-29 20:42:13 -0300

    algoritmo 'quick sort'

--- sorting algorithms/quick_sort/quick_sort.py
+++ sorting algorithms/quick_sort/quick_sort.py
@@ -1,77 +1,18 @@
-# def partition(array, left, right):
-#     pointer = array[left]
-
-#     i = left
-#     j = right
-
-#     while(i <= j):
-#         while(array[i] <= pointer):
-#             i += 1
-
-#             if(i == right):
-#                 break
-
-#         while(pointer <= array[j]):
-#             j -= 1
-            
-#             if(j == left):
-#                 break
-
-#         if(i >= j):
-#             break
-
-#         array[i], array[j] = array[j], array[i]
-
-#     pointer, array[j] = array[j], pointer
-
-#     return j
-
-# def quick_sort(array, left, right):
-#     if(left >= right):
-#         return
-
-#     index_pointer = partition(array, left, right)
-#     quick_sort(array, left, index_pointer - 1)
-#     quick_sort(array, index_pointer + 1, right)
-
-
 def partition(array, start, end):
     pivot = array[end]
-    bottom = start - 1
-    top = end
+    i = start
+    
+    for j in range(start, end):
+        if(array[j] <= pivot):
+            array[j], array[i] = array[i], array[j]
+            i += 1
+    array[i], array[end] = array[end], array[i]
+    return i
 
-    done = 0
-
-    while(not done):
-        while(not done):
-            bottom += 1
-
-            if(bottom == top):
-                done = 1
-                break
-
-            if(array[top] > pivot):
-                array[top] = array[bottom]
-                break
-
-        while(not done):
-            top -= 1
-
-            if(top == bottom):
-                done = 1
-                break
-
-            if(array[top] < pivot):
-                array[bottom] = array[top]
-                break
-
-        array[top] = pivot
-        return top
-
-def quick_sort(array, start, end):
+def quick_sort(array, start=0, end=None):
+    if(end is None):
+        end = len(array) - 1
     if(start < end):
-        split = partition(array, start, end)
-        quick_sort(array, start, split - 1)
-        quick_sort(array, split + 1, end)
-    else:
-        return
+        p = partition(array, start, end)
+        quick_sort(array, start, p - 1)
+        quick_sort(array, p + 1, end)
